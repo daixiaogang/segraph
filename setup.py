c1dcef591f14411a7871988e9ecdd3b3a1f7f585
alivcor <abhinandandubey@live.com>
2017-05-29 21:53:26 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'segraph',
+  packages = ['segraph'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Creates graphs with edges and vertices from SLIC segments',
+  author = 'Abhinandan Dubey',
+  author_email = 'abhinandandubey@live.com',
+  url = 'https://github.com/alivcor/segraph', # use the URL to the github repo
+  download_url = 'https://github.com/alivcor/segraph/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
